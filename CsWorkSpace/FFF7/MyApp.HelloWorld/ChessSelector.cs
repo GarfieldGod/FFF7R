fca61a5870002ffb18b736d0fa844e6532141ab2
caochuan <caochuan@huawei.com>
2025-03-08 18:28:18 +0800

    Ver1.0 add ChessDispenser
    Signed-off-by: caochuan <caochuan@huawei.com>

--- CsWorkSpace/FFF7/MyApp.HelloWorld/ChessSelector.cs
+++ CsWorkSpace/FFF7/MyApp.HelloWorld/ChessSelector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Chess {
     public int cost = 1;
@@ -6,32 +7,96 @@ public class Chess {
     public Tuple<int, int> chessPos = new Tuple<int, int>(0, 0);
 }
 
+public class ChessDispenser {
+    public static List<Chess> chessPool = new List<Chess>{};
+
+    public static Chess DispenseChess() {
+        if (chessPool.Count == 0) {
+            return null;
+        }
+        Random random = new Random();
+        int randomIndex = random.Next(chessPool.Count);
+        Chess chess = chessPool[randomIndex];
+        chessPool.RemoveAt(randomIndex);
+        return chess;
+    }
+
+    public static void ReDispense(List<int> index) {
+        List<Chess> popChessList = new List<Chess>{};
+        foreach(int i in index) {
+            if(i >= ChessSelector.chessList.Count) {
+                Console.WriteLine("Invaild chessIndex.");
+                continue;
+            }
+            popChessList.Add(ChessSelector.chessList[i]);
+        }
+        foreach(Chess chess in popChessList) {
+            ChessSelector.chessList.Remove(chess);
+        }
+        foreach(Chess chess in popChessList) {
+            ChessSelector.DoSendChess(DispenseChess());
+        }
+        foreach(Chess chess in popChessList) {
+            chessPool.Add(chess);
+        }
+        Console.WriteLine("ReDispense Over.");
+    }
+}
+
 public class ChessSelector {
     static Tuple<int, int> ChessSelectorPos = new Tuple<int, int>(0, 0);
-    static int ChessSelectorLength = 10;
-    static List<Chess> chessList = new List<Chess>{};
+    static int ChessSelectorLength = 100;
+    public static List<Chess> chessList = new List<Chess>{};
+    public static KeyValuePair<Chess?, int> previewChess = new KeyValuePair<Chess?, int>(null, 0);
 
     public static void DoSendChess(Chess chessOne) {
-        chessList.Add(chessOne);
-        int chessGap = ChessSelectorLength / (chessList.Count + 1);
-        Console.WriteLine("chessGap: " + chessGap);
-        for(int i = 0; i < chessList.Count ; i++) {
-            chessList[i].chessPos = new Tuple<int, int>(ChessSelectorPos.Item1 + (i + 1)*chessGap, ChessSelectorPos.Item2);
-            Console.WriteLine("chess: " + " Pos: " + chessList[i].chessPos.Item1);
+        if(chessOne == null) {
+            return;
         }
+        chessList.Add(chessOne);
         ResetAllChessPos();
     }
 
     public static void ResetAllChessPos() {
+        int chessGap = ChessSelectorLength / (chessList.Count + 1);
+        Console.WriteLine("chessGap: " + chessGap);
+        for (int i = 0; i < chessList.Count; i++) {
+            chessList[i].chessPos = new Tuple<int, int>(ChessSelectorPos.Item1 + (i + 1) * chessGap, ChessSelectorPos.Item2);
+            // Console.WriteLine("DoSendChess chess: " + i + " Pos: " + chessList[i].chessPos.Item1);
+        }
         Console.WriteLine("chessListSize: " + chessList.Count);
-        foreach(var chess in chessList) {
-            Console.WriteLine("chess: " + " Pos: " + chess.chessPos.Item1);
-            for(int i = 0; i< chess.chessPos.Item1; i++) {
+        foreach (var chess in chessList) {
+            //Console.WriteLine("ResetAllChessPos chess Pos: " + chess.chessPos.Item1);
+            for (int i = 0; i < chess.chessPos.Item1; i++) {
                 Console.Write(" ");
             }
-            // Console.Write(chess.chessPos.Item1);
             Console.Write(chess.level);
             Console.Write("\n");
         }
+    }
+
+    public static bool OutChess(Chess chess) {
+        bool result = chessList.Remove(chess);
+        ResetAllChessPos();
+        return result;
+    }
+
+    public static bool DoPreview(Chess chess) {
+        Console.WriteLine("------------------------------DoPreview");
+        previewChess = new KeyValuePair<Chess?, int>(chess, chessList.IndexOf(chess));
+        bool result = chessList.Remove(chess);
+        ResetAllChessPos();
+        return result;
+    }
+
+    public static void CancelPreview() {
+        Console.WriteLine("------------------------------CancelPreview");
+        if (previewChess.Key == null) {
+            Console.WriteLine("CancelPreview Nothing to do.");
+            return;
+        }
+        chessList.Insert(previewChess.Value, previewChess.Key);
+        previewChess = new KeyValuePair<Chess?, int>(null, 0);
+        ResetAllChessPos();
     }
 }
--- CsWorkSpace/FFF7/MyApp.HelloWorld/obj/Debug/net9.0/MyApp.HelloWorld.AssemblyInfo.cs
+++ CsWorkSpace/FFF7/MyApp.HelloWorld/obj/Debug/net9.0/MyApp.HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7a8b4c0184c0034478c775527f70a75940f4e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
